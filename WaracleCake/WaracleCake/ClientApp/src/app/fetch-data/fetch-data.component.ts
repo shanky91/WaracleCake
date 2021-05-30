b8f8d44b03d951c8bf5a2dbf5ee63494238a15fc
Sean McCafferty <shanky_91@hotmail.com>
2021-05-30 22:08:19 +0100

    Clean up unrequired template code. Implement UI to display list of all cake.

--- WaracleCake/WaracleCake/ClientApp/src/app/fetch-data/fetch-data.component.ts
+++ WaracleCake/WaracleCake/ClientApp/src/app/fetch-data/fetch-data.component.ts
@@ -3,21 +3,23 @@ import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-fetch-data',
-  templateUrl: './fetch-data.component.html'
+  templateUrl: './fetch-data.component.html',
+  styleUrls: ['./fetch-data.component.css']
 })
 export class FetchDataComponent {
-  public forecasts: WeatherForecast[];
+  public cakes: Cake[];
 
   constructor(http: HttpClient, @Inject('BASE_URL') baseUrl: string) {
-    http.get<WeatherForecast[]>(baseUrl + 'weatherforecast').subscribe(result => {
-      this.forecasts = result;
+    http.get<Cake[]>(baseUrl + 'api/cakes').subscribe(result => {
+      this.cakes = result;
     }, error => console.error(error));
   }
 }
 
-interface WeatherForecast {
-  date: string;
-  temperatureC: number;
-  temperatureF: number;
-  summary: string;
+interface Cake {
+  id: number;
+  name: string;
+  comment: string;
+  imageUrl: string;
+  yumFactor: number;
 }
